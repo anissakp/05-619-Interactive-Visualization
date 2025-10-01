ca15c6d57fffac8347eca5a41a444baf9faaa75e
Anissa Patel <anissapatel07@gmail.com>
2025-09-30 23:00:07 -0400

    aqi chart; code set-up, general comments

--- src/lib/AQIChart.svelte
+++ src/lib/AQIChart.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import * as d3 from 'd3';
 
+	// typescript interface that describes structure of each data item
 	interface Item {
 		city: string;
 		country: string;
@@ -14,6 +15,16 @@
 
 	// properties this component accepts
 	const { data }: { data: Item[] } = $props();
+
+	// provided code for AQI levels
+	[
+		{ name: 'Good', min: 0, max: 50, color: '#9cd84e' },
+		{ name: 'Moderate', min: 51, max: 100, color: '#facf39' },
+		{ name: 'Unhealthy for Sensitive Groups', min: 101, max: 150, color: '#f99049' },
+		{ name: 'Unhealthy', min: 151, max: 200, color: '#f65e5f' },
+		{ name: 'Very Unhealthy', min: 201, max: 300, color: '#a070b6' },
+		{ name: 'Hazardous', min: 301, color: '#a06a7b' }
+	];
 
 	// just for debugging; can be removed
 	$inspect(data);
